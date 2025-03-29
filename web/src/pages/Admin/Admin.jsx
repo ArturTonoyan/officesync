a5713d817ef76ba459b8f347772b9ca15345094c
Artur <artur.tonoyan.04@list.ru>
2025-03-30 02:07:38 +0300

    1

--- web/src/pages/Admin/Admin.jsx
+++ web/src/pages/Admin/Admin.jsx
@@ -1,7 +1,12 @@
 import styles from "./Admin.module.scss";
+import LeftMenu from "./LeftMenu/LeftMenu";
 
 function Admin() {
-  return <div className={styles.Admin}>Admin</div>;
+  return (
+    <div className={styles.Admin}>
+      <LeftMenu />
+    </div>
+  );
 }
 
 export default Admin;
---
+++ web/src/pages/Admin/LeftMenu/LeftMenu.jsx
@@ -0,0 +1,81 @@
+import styles from "./LeftMenu.module.scss";
+import logoIcon from "@assets/images/logo/logo.svg";
+import officeIcon from "@assets/images/leftMenu/office.svg";
+import deviceIcon from "@assets/images/leftMenu/device.svg";
+import personsIcon from "@assets/images/leftMenu/persons.svg";
+import errorsIcon from "@assets/images/leftMenu/errors.svg";
+import constructorIcon from "@assets/images/leftMenu/constructor.svg";
+import paramIcon from "@assets/images/leftMenu/param.svg";
+import userIcon from "@assets/images/leftMenu/user.svg";
+import { useNavigate } from "react-router-dom";
+function LeftMenu() {
+  const navigate = useNavigate();
+
+  const listMenu = [
+    {
+      icon: officeIcon,
+      title: "Офисы",
+      navigate: "/mainpage",
+    },
+    {
+      icon: deviceIcon,
+      title: "Оборудование",
+      navigate: "/mainpage/equipment",
+    },
+    {
+      icon: personsIcon,
+      title: "Сотрудники",
+      navigate: "/mainpage/workers",
+    },
+    {
+      icon: errorsIcon,
+      title: "Неполадки",
+      navigate: "/mainpage",
+    },
+  ];
+
+  return (
+    <div className={styles.LeftMenu}>
+      <div className={styles.logotype}>
+        <img src={logoIcon} alt="Логотип" />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.container}>
+          <div className={styles.tables_box}>
+            <span>Таблицы</span>
+            <ul className={styles.list_item}>
+              {listMenu.map((item, index) => (
+                <li key={index} onClick={() => navigate(item.navigate)}>
+                  <img src={item.icon} alt="img" />
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.constructor_box}>
+            <ul className={styles.list_item}>
+              <li>
+                <img src={constructorIcon} alt="img" />
+                <span>Конструктор</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.container_bottom}>
+          <ul className={styles.list_item}>
+            <li>
+              <img src={paramIcon} alt="img" />
+              <span>Настройки</span>
+            </li>
+            <li>
+              <img src={userIcon} alt="img" />
+              <span>Профиль</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftMenu;
